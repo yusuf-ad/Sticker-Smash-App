450a0fbfe3e61e4d4628f26af93da36fbde1eaf0
yusuf <yusufgad24@gmail.com>
2024-07-24 16:00:54 +0300

    Implement  select image functionality

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { FontAwesome } from "@expo/vector-icons";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       {/* image container */}
 
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
 
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme={"primary"} />
+        <Button
+          label="Choose a photo"
+          theme={"primary"}
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,7 +1,11 @@
 import { Image, StyleSheet } from "react-native";
 
-function ImageViewer({ placeholderImageSource }) {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource = selectedImage
+    ? { uri: selectedImage }
+    : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
