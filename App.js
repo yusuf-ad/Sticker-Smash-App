cb6d27759ac3c8224e14ccef849b94b0e23d4806
yusuf <yusufgad24@gmail.com>
2024-07-24 15:43:23 +0300

    Implement home screen

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {/* image container */}
+
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme={"primary"} />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -13,8 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
+    padding: 16,
+  },
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+
+  text: {
+    color: "#fff",
   },
 });
